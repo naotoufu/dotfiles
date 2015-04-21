41e76a34492c727db8d33486c7bf0f92ad95444c
Naoto Ito <naoto360@hotmail.co.jp>
2015-04-21 06:43:00 +0000

    test

--- mklink.sh
+++ mklink.sh
@@ -9,7 +9,7 @@ ln -sf ~/dotfiles/tmux.conf	~/.tmux.conf
 #ln -sf ~/dotfiles/xinitrc	~/.xinitrc
 ln -sf ~/dotfiles/vimrc	~/.vimrc
 #ln -sf ~/dotfiles/vim	~/.vim
-git clone https://github.com/yanaoto/bin.git
+git clone https://github.com/yanaoto/bin.git ~/bin
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim
 git clone https://github.com/Shougo/vimproc.vim.git ~/.vim/bundle/vimproc.vim
 ln -sf ~/dotfiles/emacs.d      ~/.emacs.d
@@ -18,6 +18,6 @@ ln -sf ~/dotfiles/zshrc	~/.zshrc
 ln -sf ~/dotfiles/zshrc.mine	~/.zshrc.mine
 ln -sf ~/dotfiles/zshenv	~/.zshenv
 ln -sf ~/dotfiles/zshrc_alias	~/.zshrc_alias
-sudo yum -y install zsh
+#sudo yum -y install zsh
 sudo chsh -s `which zsh`
 exec zsh -l
